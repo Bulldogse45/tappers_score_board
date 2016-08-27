733f816e9a36d65ea9a00a45000c21ecb71dfecd
Matt Fair <matt.j.fair@gmail.com>
2016-08-26 20:12:20 -0400

    cleaning up some code

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,10 +3,6 @@ class WelcomeController < ApplicationController
   def index
     @games = Game.all
     @groups = Group.all
-    respond_to do |format|
-      format.js {render index: "location.reload();" }
-      format.html {}
-    end
   end
 
 end
