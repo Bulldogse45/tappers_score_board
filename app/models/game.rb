e9f6c5956e1bfa37345d1a07f78361990313bcc1
Matt Fair <matt.j.fair@gmail.com>
2017-01-08 21:20:12 -0500

    images instead of descriptions

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,8 @@ class Game < ActiveRecord::Base
   def top_scores
     scores.where(game_id: id).order(:amount).reverse.first(10)
   end
+
+  def image_name
+    name.downcase.tr(" ", "_").gsub(/[^0-9a-z_ ]/i, '')
+  end
 end
